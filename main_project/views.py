50ed14aa3c79e33d50d7ff22344be67d173aefbe
Ilya <balashovia@bk.ru>
2021-08-30 16:59:49 +0300

    Добавлен базовый шаблон - base.html Включенные шаблоны: index.html, contacts.html, registration.html, main_menu.html, footer_menu.html> style_css.html. Футер и мейн меню похожи, но сделал отдельными файлами, потому что предполагаю, что футер будет немного другим в дальнейшем

--- main_project/views.py
+++ main_project/views.py
@@ -3,16 +3,16 @@ from iqw_framework.templator import render
 
 def index(request):
     print(request)
-    return '200 OK', render('templates/index.html', data=request.get('secret', None))
+    return '200 OK', render('index.html', data=request.get('secret', None))
 
 
 def contacts(request):
     print(request)
-    return '200 OK', render('templates/contacts.html', data=request.get('key', None))
+    return '200 OK', render('contacts.html', data=request.get('key', None))
 
 def registration(request):
     print(request)
-    return '200 OK', render('templates/registration.html', data=request.get('data', None))
+    return '200 OK', render('registration.html', data=request.get('data', None))
 
 
 def not_found_404_view(request):
